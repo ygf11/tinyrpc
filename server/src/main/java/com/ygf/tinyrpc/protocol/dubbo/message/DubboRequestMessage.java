69c30ab6bbe9a9c6b84f7e4be0c8cc7bc82c6af7
theo <yanggf23@gmail.com>
2018-11-28 11:17:00 +0800

    fix bugs

--- server/src/main/java/com/ygf/tinyrpc/protocol/dubbo/message/DubboRequestMessage.java
+++ server/src/main/java/com/ygf/tinyrpc/protocol/dubbo/message/DubboRequestMessage.java
@@ -10,6 +10,22 @@ import java.util.List;
  */
 public class DubboRequestMessage {
     /**
+     * 请求序列号标识
+     */
+    private int requestId;
+    /**
+     * 协议名
+     */
+    private String protocal;
+    /**
+     * 协议版本号
+     */
+    private byte version;
+    /**
+     * 请求类型
+     */
+    private byte type;
+    /**
      * 服务名：全限定类名+方法名
      */
     private String serviceName;
@@ -50,5 +66,37 @@ public class DubboRequestMessage {
 
     public List<Object> getParams() {
         return params;
+    }
+
+    public void setRequestId(int requestId) {
+        this.requestId = requestId;
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public void setProtocal(String protocal) {
+        this.protocal = protocal;
+    }
+
+    public String getProtocal() {
+        return protocal;
+    }
+
+    public void setType(byte type) {
+        this.type = type;
+    }
+
+    public byte getType() {
+        return type;
+    }
+
+    public void setVersion(byte version) {
+        this.version = version;
+    }
+
+    public byte getVersion() {
+        return version;
     }
 }
--- server/src/main/java/com/ygf/tinyrpc/protocol/dubbo/message/DubboResponseMessage.java
+++ server/src/main/java/com/ygf/tinyrpc/protocol/dubbo/message/DubboResponseMessage.java
@@ -9,11 +9,23 @@ public class DubboResponseMessage {
     /**
      * 请求序号
      */
-    private Integer requestId;
+    private int requestId;
     /**
      * 协议类型
      */
     private String protocol;
+    /**
+     * 协议版本
+     */
+    private int version;
+    /**
+     * 返回类型 0-正常 1-抛出异常
+     */
+    private int type;
+    /**
+     * 如果返回类型为抛出一个异常 则此项为异常类名  否则此项不存在
+     */
+    private String exceptionClassName;
     /**
      * 结果数据
      */
