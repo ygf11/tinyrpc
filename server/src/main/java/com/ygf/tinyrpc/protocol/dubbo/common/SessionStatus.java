d0bee9bf43c188e1d4a55ff2ac7ab380baf2add0
theo <yanggf23@gmail.com>
2018-11-30 11:57:55 +0800

    add session status

---
+++ server/src/main/java/com/ygf/tinyrpc/protocol/dubbo/common/SessionStatus.java
@@ -0,0 +1,23 @@
+package com.ygf.tinyrpc.protocol.dubbo.common;
+
+/**
+ * 会话状态
+ *
+ * @author theo
+ * @date 20181130
+ */
+public class SessionStatus {
+    /**
+     * 客户端正在与服务器进行连接
+     */
+    public static int CONNECTING = 1;
+    /**
+     * 服务器收到客户端的确认后
+     */
+    public static int CONNECTED = 2;
+    /**
+     * 客户端发出退出session请求，或者在一定时间内没有发送心跳后
+     * 服务器端断开连接的状态
+     */
+    public static int DISCONNECT = 3;
+}
