3f10c9bc15d3f04c30e60f40979c4e5adec041db
theo <yanggf23@gmail.com>
2018-12-06 19:11:37 +0800

    add sessionManager

--- server/src/main/java/com/ygf/tinyrpc/protocol/jessie/common/SessionManager.java
+++ server/src/main/java/com/ygf/tinyrpc/protocol/jessie/common/SessionManager.java
@@ -1,5 +1,8 @@
 package com.ygf.tinyrpc.protocol.jessie.common;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * 服务器端会话管理类
  *
@@ -7,5 +10,26 @@ package com.ygf.tinyrpc.protocol.jessie.common;
  * @date 20181206
  */
 public class SessionManager {
+    /**
+     * 服务器端保存的会话信息
+     */
+    private static Map<Integer, ClientInfo> sessions = new ConcurrentHashMap<Integer, ClientInfo>();
 
+    /**
+     * 添加/更新会话
+     * @param sessionId
+     * @param info
+     */
+    public static void addSession(int sessionId, ClientInfo info){
+        sessions.put(sessionId, info);
+    }
+
+    /**
+     * 获取会话信息
+     * @param sessionId
+     * @return
+     */
+    public static ClientInfo getSession(int sessionId){
+        return sessions.get(sessionId);
+    }
 }
