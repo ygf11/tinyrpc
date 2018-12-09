205bc57c7a3a34941bde488692496bcde318ea3e
theo <yanggf23@gmail.com>
2018-12-09 23:57:46 +0800

    add rpcClient

---
+++ server/src/main/java/com/ygf/tinyrpc/rpc/client/RpcClent.java
@@ -0,0 +1,16 @@
+package com.ygf.tinyrpc.rpc.client;
+
+import io.netty.channel.Channel;
+
+/**
+ * 消费者端协议无关的通信类
+ *
+ * @author theo
+ * @date 20181209
+ */
+public class RpcClent {
+    /**
+     * netty通信channel
+     */
+    private Channel channel;
+}
