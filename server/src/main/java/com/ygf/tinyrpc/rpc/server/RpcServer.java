1cdf8c6ca78839b06fda6993ad1bfe5a17e9a379
theo <yanggf23@gmail.com>
2018-12-21 15:02:37 +0800

    fix logback

---
+++ server/src/main/java/com/ygf/tinyrpc/rpc/server/RpcServer.java
@@ -0,0 +1,15 @@
+package com.ygf.tinyrpc.rpc.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 服务提供端监听网络连接的启动类
+ *
+ * @author theo
+ * @date 20181220
+ */
+public class RpcServer {
+    private static Logger logger = LoggerFactory.getLogger(RpcServer.class);
+
+}
