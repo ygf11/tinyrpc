57b11b04b9758c83f00a3c1d1bce3727c3b75699
theo <yanggf23@gmail.com>
2019-02-21 20:11:15 +0800

    add utils

--- server/src/main/java/com/ygf/tinyrpc/context/RpcContext.java
+++ server/src/main/java/com/ygf/tinyrpc/context/RpcContext.java
@@ -1,5 +1,10 @@
 package com.ygf.tinyrpc.context;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * rpc上下文
  *
@@ -7,5 +12,11 @@ package com.ygf.tinyrpc.context;
  * @date 20190220
  */
 public class RpcContext {
+    private static Logger logger = LoggerFactory.getLogger(RpcContext.class);
+    /**
+     * 保存服务接口-->代理服务的映射
+     */
+    private ConcurrentHashMap<Class, Object> service = new ConcurrentHashMap<Class, Object>();
+
 
 }
---
+++ server/src/main/java/com/ygf/tinyrpc/utils/RpcContextUtils.java
@@ -0,0 +1,10 @@
+package com.ygf.tinyrpc.utils;
+
+/**
+ * rpc上下文工具类
+ *
+ * @author theo
+ * @date 20190221
+ */
+public class RpcContextUtils {
+}
